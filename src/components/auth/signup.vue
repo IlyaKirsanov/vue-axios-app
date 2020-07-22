4fd35dfbe57a081314169ff71df9048ad23a9cf5
IlyaKirsanov <ilya.kirsanov@gmail.com>
2020-07-22 15:57:09 +0300

    validate ui feedback

--- src/components/auth/signup.vue
+++ src/components/auth/signup.vue
@@ -2,15 +2,16 @@
   <div id="signup">
     <div class="signup-form">
       <form @submit.prevent="onSubmit">
-        <div class="input">
+        <div class="input" :class="{ invalid: $v.email.$error }">
           <label for="email">Mail</label>
           <input
             type="email"
             id="email"
             v-model="email"
-            @input="$v.email.$touch()"
+            @blur="$v.email.$touch()"
           />
-          <div>{{ $v }}</div>
+          <p v-if="!$v.email.email">Please provide a valid email</p>
+          <p v-if="!$v.email.required">This field must not be empty</p>
         </div>
         <div class="input">
           <label for="age">Your Age</label>
@@ -162,6 +163,15 @@ export default {
 .input select {
   border: 1px solid #ccc;
   font: inherit;
+}
+
+.input.invalid input{
+  border: 1px solid red;
+  background-color: lightcoral;
+}
+
+.input.invalid label {
+  color: red;
 }
 
 .hobbies button {
